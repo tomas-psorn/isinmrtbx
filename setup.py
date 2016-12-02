8bf333fb636a1cd72e50de24d461b35fcfd6a539
Tomas Psorn <tomas.psorn@gmail.com>
2016-12-02 15:56:15 +0100

    examples added to setup

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
 setup(name='isinmrtbx',
-      version='0.6.2',
+      version='0.6.3',
       description='Tools for routine MRI data manipulation',
       author='Tomas Psorn',
       author_email='tomaspsorn@isibrno.cz',
       url='https://www.isibrno.cz',
       packages=['isinmrtbx.datatypes', 'isinmrtbx.inout' ,'isinmrtbx.tools'],
       install_requires=['numpy', 'matplotlib', 'scipy'],
+      data_files=[('examples',['isinmrtbx/examples/data/ute/*'])],
       license='GPL',
       zip_safe=False
      )
