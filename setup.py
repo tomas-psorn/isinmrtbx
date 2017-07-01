83556728b9e77c7b7dea809ca1046fc5e3d7c15b
tomas-psorn <tomas.psorn@gmail.com>
2017-07-01 21:11:52 +0200

    load diffusion

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='isinmrtbx',
-      version='0.6.7.2',
+      version='0.6.7.3',
       description='Tools for routine MRI data manipulation',
       author='Tomas Psorn',
       author_email='tomaspsorn@isibrno.cz',
